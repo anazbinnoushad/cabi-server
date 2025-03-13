b0b8e4bf8740da2be5feb1191f649fe3e7c718ff
Anaz N <anazbinnoushad@gmail.com>
2025-03-13 23:46:50 +0530

    Add user auth middleware

---
+++ src/middleware/userAuth.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "../config";
+
+export const userAuth = (req: Request, res: Response, next: NextFunction) => {
+    const authorization = req.headers.authorization
+    const token = authorization?.split(" ")[1]
+
+    if (token) {
+        const verified = jwt.verify(token, JWT_SECRET) as { id: string }
+        if (verified) {
+            req.userId = verified.id
+            next()
+        } else {
+            res.status(401).json({ message: "Failed to authenticate" })
+        }
+    } else {
+        res.status(401).json({ message: "No token found" })
+    }
+}
