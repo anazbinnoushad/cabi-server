bcc4758605238a0ba0834dd59b0bd282ad9626c4
Anaz N <anazbinnoushad@gmail.com>
2025-03-15 23:39:13 +0530

    Update error handler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,11 +1,20 @@
-import {Request, Response} from 'express';
+import {NextFunction, Request, Response} from 'express';
 
 interface CustomError extends Error {
     status?: number;
 }
 
-export const errorHandler = (err: CustomError, req: Request, res: Response) => {
+export const errorHandler = (
+    err: CustomError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
     const statusCode = err.status || 500;
+
+    if (req.path.startsWith('/api/v1/user')) {
+        return next();
+    }
 
     res.status(statusCode).json({
         success: false,
